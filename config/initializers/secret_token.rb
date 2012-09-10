257edb75c805b1fb54847a00ab95ad3f6250f898
Mauricio Aniche <mauricioaniche@gmail.com>
2012-09-10 20:18:57 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tddnomundoreal::Application.config.secret_token = '5ae185cbb06b36b0074890c519a3d98a0383588f5243845567d1d1b374ea437bd1175765aea815c820241a7c3b2e3cf9091fa2124872c6bee08cccf7e22377d7'
